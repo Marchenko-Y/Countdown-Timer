c22479947611b29ad3276c2ad59fabb97bf14a89
marchenko ulya <marchenko.ulya26@gmail.com>
2020-01-23 21:42:58 +0300

    Added form  with event and eventDate, function handleSubmit

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -1,17 +1,14 @@
 import React from "react";
 
 class Countdown extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      event: "Hello everyone!",
-      date: "2020-01-31",
-      days: undefined,
-      hours: undefined,
-      minutes: undefined,
-      seconds: undefined
-    };
-  }
+  state = {
+    event: "New year",
+    eventDate: "2021-01-01",
+    days: undefined,
+    hours: undefined,
+    minutes: undefined,
+    seconds: undefined
+  };
 
   calculateTimeToEvent(eventDate) {
     const difference = new Date(eventDate) - new Date();
@@ -24,25 +21,59 @@ class Countdown extends React.Component {
       });
     }
   }
+
   componentDidMount() {
     this.timer = setInterval(() => {
-      this.calculateTimeToEvent(this.state.date);
+      this.calculateTimeToEvent(this.state.eventDate);
     }, 1000);
   }
   componentWillUnmount() {
     clearInterval(this.timer);
   }
+  handleSubmit(e) {
+    this.setState({
+      event: e.target.elements.event.value,
+      eventDate: e.target.elements.eventDate.value
+    });
+    e.preventDefault();
+  }
   render() {
     const { event, days, hours, minutes, seconds } = this.state;
     return (
       <div>
-        <h1>Таймер</h1>
-        <span>Event:</span>
-        <div>{event}</div>
-        <div>{days}</div>
-        <div>{hours}</div>
-        <div>{minutes}</div>
-        <div>{seconds}</div>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Event:
+            <input type="text" name="event" />
+          </label>
+
+          <label>
+            Date:
+            <input type="date" name="eventDate" />
+          </label>
+          <input type="submit" value="go" />
+        </form>
+
+        <h1>Left before the {event}:</h1>
+        <div className="countdown-container">
+          <div className="countdown-item">
+            <div className="countdown-timer">{days}</div>
+            <span>days</span>
+          </div>
+          <div className="countdown-item">
+            <div className="countdown-timer">{hours}</div>
+            <span>hours</span>
+          </div>
+          <div className="countdown-item">
+            <div className="countdown-timer">{minutes}</div>
+
+            <span>minutes</span>
+          </div>
+          <div className="countdown-item">
+            <div className="countdown-timer">{seconds}</div>
+            <span>seconds</span>
+          </div>
+        </div>
       </div>
     );
   }
