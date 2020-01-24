5e340a0a6b8190b63552cb9b81046fc4771b5ea3
marchenko ulya <marchenko.ulya26@gmail.com>
2020-01-24 20:16:45 +0300

    Fixed bugs and aded new styles

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 
 class Countdown extends React.Component {
-  state = {
-    event: "New year",
-    eventDate: "2021-01-01",
-    days: undefined,
-    hours: undefined,
-    minutes: undefined,
-    seconds: undefined
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      event: "New year",
+      eventDate: "2021-01-01",
+      days: undefined,
+      hours: undefined,
+      minutes: undefined,
+      seconds: undefined
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
   calculateTimeToEvent(eventDate) {
     const difference = new Date(eventDate) - new Date();
@@ -18,6 +23,13 @@ class Countdown extends React.Component {
         hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
         minutes: Math.floor((difference / 1000 / 60) % 60),
         seconds: Math.floor((difference / 1000) % 60)
+      });
+    } else {
+      this.setState({
+        days: "D",
+        hours: "O",
+        minutes: "N",
+        seconds: "E"
       });
     }
   }
@@ -40,38 +52,41 @@ class Countdown extends React.Component {
   render() {
     const { event, days, hours, minutes, seconds } = this.state;
     return (
-      <div>
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Event:
-            <input type="text" name="event" />
-          </label>
+      <div className="wrapper">
+        <div className="container">
+          <form onSubmit={this.handleSubmit} className="countdown-form">
+            <label>
+              Event:
+              <input type="text" name="event" className="event" />
+            </label>
 
-          <label>
-            Date:
-            <input type="date" name="eventDate" />
-          </label>
-          <input type="submit" value="go" />
-        </form>
+            <label>
+              Date:
+              <input type="date" name="eventDate" className="eventDate" />
+            </label>
+            <input type="submit" value="go" />
+          </form>
 
-        <h1>Left before the {event}:</h1>
-        <div className="countdown-container">
-          <div className="countdown-item">
-            <div className="countdown-timer">{days}</div>
-            <span>days</span>
-          </div>
-          <div className="countdown-item">
-            <div className="countdown-timer">{hours}</div>
-            <span>hours</span>
-          </div>
-          <div className="countdown-item">
-            <div className="countdown-timer">{minutes}</div>
-
-            <span>minutes</span>
-          </div>
-          <div className="countdown-item">
-            <div className="countdown-timer">{seconds}</div>
-            <span>seconds</span>
+          <h1 className="eventTitle">
+            Left before <span>the {event}</span>:
+          </h1>
+          <div className="countdown-container">
+            <div className="countdown-item">
+              <div className="countdown-timer">{days}</div>
+              {days === 1 ? <span>day</span> : <span>days</span>}
+            </div>
+            <div className="countdown-item">
+              <div className="countdown-timer">{hours}</div>
+              {hours === 1 ? <span>hour</span> : <span>hours</span>}
+            </div>
+            <div className="countdown-item">
+              <div className="countdown-timer">{minutes}</div>
+              {minutes === 1 ? <span>minute</span> : <span>minutes</span>}
+            </div>
+            <div className="countdown-item">
+              <div className="countdown-timer">{seconds}</div>
+              {seconds === 1 ? <span>second</span> : <span>seconds</span>}
+            </div>
           </div>
         </div>
       </div>
